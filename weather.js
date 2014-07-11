4bb8fbde3c6ae7af9de854fd233f357ee01378be
abccbaandy <abccbaandy@vm-0.abccbaandy.koding.kd.io>
2014-07-11 06:51:55 +0000

    clean code

--- weather.js
+++ weather.js
@@ -1,42 +1,46 @@
-$('.dropdown-toggle').dropdown();
+var weatherImg;
+var gpsImg = loadImage('gps.png', main);
+
+//dummy geo data
 var latitude = 23;
 var longitude = 200;
 
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
-
 var cwb_path = [];
+//not use anymore
 var cwb_name = [];
+
+var refreshIntervalId;
+
+var selectedImgIndex = 0;
+
+var selectedImg;
+
 $("#sel").addOption(MOS2_1024N, false);
+
 for (var key in MOS2_1024N) {
     cwb_path.push(key);
     cwb_name.push(MOS2_1024N[key]);
 }
 
-var img1 = [];
-/*img1[0] = loadImage('http://www.cwb.gov.tw'+cwb_path[2], main);
-img1[1] = loadImage('http://www.cwb.gov.tw'+cwb_path[1], main);
-img1[2] = loadImage('http://www.cwb.gov.tw'+cwb_path[0], main);
-*/
-var img2 = loadImage('gps.png', main);
-
-
-
-
-var imagesLoaded = 0;
-//var counter = 0;
 navigator.geolocation.getCurrentPosition(GetLocation);
+
 function GetLocation(location) {
-    alert(location.coords.latitude);
     alert(location.coords.longitude);
-    //alert(location.coords.accuracy);
+    alert(location.coords.latitude);
+    alert(location.coords.accuracy);
+    
+    //(x-min)/(max-min)=(xImg-minImg)/(maxImg-minImg)
 	latitude = 476-(location.coords.latitude-23.474)/(25.47-23.474)*476;
-	//longitude = location.coords.longitude+240;
 	longitude = (location.coords.longitude-120)/(121.998-120)*476;
 	//alert("GetLocation  "+latitude);
-	ctx.drawImage(img2, longitude, latitude);
+	
+	//ctx.globalAlpha = 0.7;
+	ctx.drawImage(gpsImg, longitude, latitude);
 }
+
 function loadImage(src, onload) {
     // http://www.thefutureoftheweb.com/blog/image-onload-isnt-being-called
     var img = new Image();
@@ -46,85 +50,47 @@ function loadImage(src, onload) {
 
     return img;
 }
+
 function main() {
-    imagesLoaded += 1;
-
-    /*if(imagesLoaded == 2) {
-        // composite now
-        ctx.drawImage(img1, 0, 0);
-        
-        //ctx.globalAlpha = 1;
-        ctx.drawImage(img2, longitude, latitude);
-    }*/
-    //window.setTimeout(imageLoop, 1000);
-    //ctx.drawImage(img2, longitude, latitude);
-    if(imagesLoaded == 4) {
-        //alert("Image is loaded");
-        //setInterval(drawScreen(), 1000);
-    }
-    //imageLoop();
+    //do nothing
 }
 
-function drawScreen() {
-    // composite now
-    ctx.drawImage(img1[counter], 0, 0);
-    
-    if(counter==2)
-        counter = 0;
-    else
-        counter++;
-        
-    //ctx.globalAlpha = 0.7;
-    ctx.drawImage(img2, longitude, latitude);
-    
-    var d = new Date();
-    var t = d.toLocaleTimeString();
-    document.getElementById("time").innerHTML = t;
-}
-function imageLoop() {
-    //window.setTimeout(function(){imageLoop()}, 1000000000);
-    //drawScreen();
-    setInterval(function(){drawScreen()}, 1000);
-}
-var refreshIntervalId;
+//select change listener
 function selchange() {
+    //stop current loop
     clearInterval(refreshIntervalId);
     //alert(document.getElementById("sel").selectedIndex);
-    img3 = loadImage('http://www.cwb.gov.tw'+cwb_path[document.getElementById("sel").selectedIndex], selchangedraw);
-    //ctx.drawImage(img3, 0, 0);
-    //ctx.drawImage(img2, longitude, latitude);
-}
-function selchangedraw() {
-    ctx.drawImage(img3, 0, 0);
-    ctx.drawImage(img2, longitude, latitude);
+    weatherImg = loadImage('http://www.cwb.gov.tw'+cwb_path[document.getElementById("sel").selectedIndex], drawImg);
 }
 
-var counter = 0;
-function drawSelectImg() {
-    // composite now
-    img3 = loadImage('http://www.cwb.gov.tw'+cwb_path[selected[selected.length-1-counter]], selchangedraw);
-    
-    if(counter==selected.length-1)
-        counter = 0;
-    else
-        counter++;
-        
+function drawImg() {
+    ctx.drawImage(weatherImg, 0, 0);
     //ctx.globalAlpha = 0.7;
-    ctx.drawImage(img2, longitude, latitude);
-    /*
-    var d = new Date();
-    var t = d.toLocaleTimeString();
-    document.getElementById("time").innerHTML = t;
-    */
+    ctx.drawImage(gpsImg, longitude, latitude);
 }
 
-function getSelectValues() {
-  clearInterval(refreshIntervalId);
-  selected = [];
-  for (var i = 0; i < document.getElementById("sel").options.length; i++) {
-    if (document.getElementById("sel").options[ i ].selected)
-      selected.push(document.getElementById("sel").options[ i ].index);
-  }
-  //alert(selected);
-  refreshIntervalId = setInterval(function(){drawSelectImg()}, 1000);
+function drawLoopImg() {
+    // composite now
+    weatherImg = loadImage('http://www.cwb.gov.tw'+cwb_path[selectedImg[selectedImg.length-1-selectedImgIndex]], drawImg);
+    
+    if(selectedImgIndex==selectedImg.length-1)
+        selectedImgIndex = 0;
+    else
+        selectedImgIndex++;
+}
+
+function startLoop() {
+    //stop current loop
+    clearInterval(refreshIntervalId);
+    //reset loop counter
+    selectedImgIndex = 0;
+    //re-new array
+    selectedImg = [];
+    for (var i = 0; i < document.getElementById("sel").options.length; i++) {
+        if (document.getElementById("sel").options[ i ].selected)
+            selectedImg.push(document.getElementById("sel").options[ i ].index);
+    }
+    //alert(selectedImg);
+    //start loop
+    refreshIntervalId = setInterval(function(){drawLoopImg()}, 1000);
 }
